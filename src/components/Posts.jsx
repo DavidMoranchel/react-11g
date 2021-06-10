9301b6618429ec98a78eb24104f0900586158572
David Moranchel <david.arcshtag@gmail.com>
2021-06-09 20:29:51 -0500

    feat: Fetch API + data buss

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,48 +2,49 @@ import React from "react";
 
 import AppCard from "./AppCard";
 
+const data = [
+  {
+    title: "Card title 1",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    title: "Card title 2",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    title: "Card title 3",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+  {
+    title: "Card title 4",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+];
+
 class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  buildLI({ id, name, species }) {
+    return (
+      <li key={id}>
+        <AppCard>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">Species: {species}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </AppCard>
+      </li>
+    );
+  }
+
   render() {
+    console.log(this.props);
     return (
       <ul className="d-flex flex-column">
-        <li>
-          <AppCard>
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </AppCard>
-        </li>
-
-        <li>
-          <AppCard>
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </AppCard>
-        </li>
-
-        <li>
-          <AppCard>
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </AppCard>
-        </li>
+        {this.props.data.map(this.buildLI)}
       </ul>
     );
   }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import AppLoading from "../../components/AppLoading";
 import Header from "../../components/Header";
 import SideNavLeft from "../../components/SideNavLeft";
 import SideNavRight from "../../components/SideNavRight";
 import Posts from "../../components/Posts";
-import Counter from "../../components/Counter";
 
 export default function Home() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  console.log(data.results);
+
   return (
     <React.Fragment>
       <Header />
@@ -15,8 +24,7 @@ export default function Home() {
         <div className="row">
           <SideNavLeft />
           <main className="col-8">
-            <Posts />
-            <Counter />
+            <Posts data={data.results ? data.results : []} />
           </main>
           <SideNavRight />
         </div>
@@ -25,3 +33,13 @@ export default function Home() {
     </React.Fragment>
   );
 }
+
+// const getCharacters = async () => {
+//   const response = await fetch("https://rickandmortyapi.com/api/character")
+//   const data = await response.json()
+//   return data
+// }
+
+// fetch("https://rickandmortyapi.com/api/character?page=2")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
